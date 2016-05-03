7461ba462aca66aa1ad03325106af5d0df9770e0
Michael Anthony Teti <mtetifau@gmail.com>
2016-05-03 08:34:55 -0400

    Dictionary Update

--- dict_update.m
+++ dict_update.m
@@ -1,16 +1,20 @@
-function r=dict_update(X, D_temp, alpha_temp)
+function dict_update(X, D_temp, alpha_temp)
 
 Y=[];
 alpha_comp=[];
-[p ~]=find(alpha_temp~=0);
-for j=1:length(p);
-	Xcols=X(:, p(j))';
-	Y=[Y; Xcols];
-	alpha_comp=[alpha_comp; alpha_temp(p(j))'];
-end
-	
-r=alpha_comp*D_temp-Y;
-r=sum(sum(r.^2));
+[p ~]=find(alpha_temp~=0);  %Find only those dictionary patches that were used in reconstruction
+Xcols=X(:, p)';  %Find signals that used that dictionary patch
+Y=[Y; Xcols];
+alpha_comp=[alpha_comp; alpha_temp(p)'];
+
+cvx_begin;	
+    minimize( sum(sum((alpha_comp*D_temp'-Y).^2)) );  %minimize difference between error 
+    subject to;
+        alpha_comp==alpha_comp;  %Keep alpha the same, just modify the dictionary atom
+        Y==Y;
+cvx_end;
+
 
 end
+
 
